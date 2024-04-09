d49e81669fb9bbd64302925dd9c61cec76a72677
LBouzac <bouzacludovic@gmail.com>
2024-04-09 09:33:15 +0200

    ajout int

--- guess.py
+++ guess.py
@@ -1,6 +1,8 @@
 import itertools
+from tkinter import END
 
-def guess_password(real):
+
+def guess_password(real, password_display):
     chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890&é"(-è_çà)=\'$*ù%£µ!:;,?./§^#@<>|`[]{}\\'
     attempts = 0
     for password_length in range(1, 64):
@@ -9,4 +11,5 @@ def guess_password(real):
             guess = ''.join(guess)
             if guess == real:
                 return 'MDP est {}. trouvé en {} essaies.'.format(guess, attempts)
-            print(guess, attempts)
+            password_display.insert(END, f'{guess}\n')  # Met à jour le widget Text avec le nouveau mot de passe
+            password_display.see(END)  # Fait défiler le widget Text pour montrer le dernier mot de passe
--- interface.py
+++ interface.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from tkinter import Label
-
+from tkinter import Label, Text, END
 
 def create_window():
     window = tk.Tk()  # Crée une nouvelle fenêtre
@@ -10,16 +9,24 @@ def create_window():
     button = tk.Button(window, text="Clique", command=on_button_click)
     button.pack()
 
-    monAffichage = Label(window, text="C’est ici que j’affiche mon premier texte !")
+    button_exit = tk.Button(window, text="Exit", command=exit_program)  # Utilisez le nouveau nom de la fonction ici
+    button_exit.pack()
 
+    monAffichage = Label(window, text="C’est ici que j’affiche mon premier texte !")
     monAffichage.pack()
 
+    # Ajoutez ce widget Text
+    global password_display
+    password_display = Text(window)
+    password_display.pack()
 
     window.mainloop()
 
 def on_button_click():
     import guess
-    print(guess.guess_password('Aa'))
+    guess.guess_password('Aa', password_display)
 
+def exit_program():  # Renommez la fonction ici
+    exit()
 
 create_window()
