70d8f1651668029880b2a03de710ef500860abe1
LBouzac <bouzacludovic@gmail.com>
2024-06-03 14:19:53 +0200

    maj

--- README.md
+++ README.md
@@ -8,3 +8,32 @@ Ce projet a pour but de réaliser un programme en Python qui permet de tester la
 - pip
 - Git
 - GitHub
+# detail comme si tu le presentais a un client
+## Installation
+Pour installer le programme, il suffit de cloner le projet sur votre machine en utilisant la commande suivante:
+```bash
+git clone
+```
+Ensuite, il faut installer les dépendances du projet en utilisant la commande suivante:
+```bash
+pip install -r requirements.txt
+```
+## Utilisation
+Pour utiliser le programme, il suffit de lancer le fichier main.py en utilisant la commande suivante:
+```bash
+python main.py
+```
+# liste les dependances utilisees dans le projet
+## Dépendances
+- itertools
+- threading
+- time
+- requests
+- tkinter
+- os
+- ctypes
+
+Ensuite, il suffit de suivre les instructions affichées dans la console pour tester la force d'un mot de passe.
+## Auteur
+- Nom: Bouzac
+- Prénom: Ludovic
--- guess.py
+++ guess.py
@@ -5,9 +5,9 @@ import requests
 from tkinter import END
 
 MAX_PASSWORD_LENGTH = 64
-CHARS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890&é"(-è_çà)=\'$*ù%£µ!:;,?./§^#@<>|`[]{}\\'
+CHARS = 'abcdefgZ1234'
 
-url = 'http://localhost/B2SLAM-AppRestoWeb/connexion.php'
+url = 'http://localhost/projets/B2SLAM-AppRestoWeb/connexion.php'
 
 
 def guess_password(real, password_display):
@@ -20,14 +20,14 @@ def guess_password(real, password_display):
         attempts = 0
         start_time = time.perf_counter()
         try:
-            for password_length in range(8, MAX_PASSWORD_LENGTH):
+            for password_length in range(4, MAX_PASSWORD_LENGTH):
                 for guess in itertools.product(CHARS, repeat=password_length):
                     attempts += 1
                     guess = ''.join(guess)
 
                     data = {
-                        'login': 'c',
-                        'password': guess,  # mdp a trouver 9JO$hvo8aFm9Eves
+                        'login': 'test',
+                        'password': guess,
                         'submit': 'submit',
                     }
 
@@ -36,8 +36,7 @@ def guess_password(real, password_display):
                     response_url = response.url
 
                     print(response_url)
-
-                    if response_url != 'http://localhost/B2SLAM-AppRestoWeb/connexion.php':
+                    if response_url != 'http://localhost/projets/B2SLAM-AppRestoWeb/connexion.php':
                         end_time = time.perf_counter()
                         time_taken = round(end_time - start_time, 3)
                         result = f'\n\nLe mot de passe est {guess}. \nTrouvé après {attempts} tentatives. \nDurée: {time_taken} secondes.\n'
@@ -51,4 +50,4 @@ def guess_password(real, password_display):
                 update_display(f'Finis après {attempts} tentatives\n')
 
     thread = threading.Thread(target=run)
-    thread.start()  # Start the thread
+    thread.start()
